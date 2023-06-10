c4f61ce26ce77d2e257f464ef2277f812ffb7d58
Sanjeev Kumar <91529367+Sanjeev718191@users.noreply.github.com>
2023-06-10 13:16:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// app.use((req, res, next) => {
+//     console.log("middleware ran");
+//     req.title = "Meeraj";
+//     next();
+// });
+
+app.use(morgan('dev'));
+app.use(express.json({}));
+app.use(express.json({
+    extended : true
+}));
+
+dotenv.config({
+    path:'./config/config.env'
+});
+
+connectDB();
+
+//http://localhost:3000/api/todo/auth/register
+app.use('/api/todo/auth', require('./routes/user'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running on port: ${PORT}`.red.underline.bold));
